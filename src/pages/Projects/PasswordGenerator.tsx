29eec3eb66dd3c5d63ca074efcb303757a173a85
Dylan Beckwith <dylan.beckwith@dialexa.com>
2023-02-09 12:35:01 -0600

    PWRX-135: Add generateCryptic method

--- src/pages/Projects/PasswordGenerator.tsx
+++ src/pages/Projects/PasswordGenerator.tsx
@@ -9,9 +9,73 @@ import {
 } from "@mui/material";
 import { Footer } from "components/Footer";
 import { Menu } from "components/Menu";
+import React from "react";
 import mainTheme from "themes/mainTheme";
 
 export function PasswordGenerator() {
+  const [password, setPassword] = React.useState("");
+
+  let uppers = Array.from(Array(26))
+    .map((e, i) => i + 65)
+    .map((x) => String.fromCharCode(x));
+
+  let lowers = Array.from(Array(26))
+    .map((e, i) => i + 97)
+    .map((x) => String.fromCharCode(x));
+
+  const specials1 = Array.from(Array(15))
+    .map((e, i) => i + 33)
+    .map((x) => String.fromCharCode(x));
+
+  const specials2 = Array.from(Array(7))
+    .map((e, i) => i + 58)
+    .map((x) => String.fromCharCode(x));
+
+  const specials3 = Array.from(Array(6))
+    .map((e, i) => i + 91)
+    .map((x) => String.fromCharCode(x));
+
+  const specials4 = Array.from(Array(4))
+    .map((e, i) => i + 123)
+    .map((x) => String.fromCharCode(x));
+
+  let specials = specials1
+    .concat(specials2)
+    .concat(specials3)
+    .concat(specials4);
+
+  let numbers = [...Array(10).keys()];
+
+  let chars = [uppers, lowers, specials, numbers];
+
+  const shuffle = (arr) => {
+    return arr
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+  };
+
+  const generateCryptic = () => {
+    let passArr = [];
+    for (let i = 0; i < chars.length; i++) {
+      for (let j = 0; j < 3; j++) {
+        const char = shuffle(chars[i])[0];
+        passArr.push(char);
+      }
+    }
+    let genPassword;
+    do {
+      genPassword = shuffle(passArr).join("");
+    } while (!specials.includes(genPassword[0]));
+    setPassword(genPassword);
+  };
+
+  const generateHuman = () => {};
+
+  const onGenerate = () => {
+    generateCryptic();
+  };
+
   return (
     <Grid container direction="column" border="double thick black">
       <ThemeProvider theme={mainTheme}>
@@ -62,10 +126,14 @@ export function PasswordGenerator() {
                 color: "white",
               },
             }}
+            onClick={onGenerate}
           >
             GENERATE
           </Button>
         </Grid>
+        <Typography color="white" variant="h6">
+          {password}
+        </Typography>
       </Grid>
       <Footer backgroundColor="black"></Footer>
     </Grid>
