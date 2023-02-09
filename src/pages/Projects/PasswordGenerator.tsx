6dcebf9365e02f82e1dfecb8c3d8cbeb6464a500
Dylan Beckwith <dylan.beckwith@dialexa.com>
2023-02-09 09:30:28 -0600

    PWRX-135: Add selector and button

--- src/pages/Projects/PasswordGenerator.tsx
+++ src/pages/Projects/PasswordGenerator.tsx
@@ -1,4 +1,12 @@
-import { Grid, ThemeProvider, Typography } from "@mui/material";
+import {
+  Button,
+  FormControlLabel,
+  Grid,
+  Radio,
+  RadioGroup,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
 import { Footer } from "components/Footer";
 import { Menu } from "components/Menu";
 import mainTheme from "themes/mainTheme";
@@ -14,10 +22,50 @@ export function PasswordGenerator() {
         width="99%"
         margin="0 auto .25rem auto"
         bgcolor="black"
+        textAlign="center"
       >
-        <Typography color="white" textAlign="center" variant="h4">
+        <Typography color="white" variant="h4">
           RANDOM PASSWORD GENERATOR
         </Typography>
+        <Grid container bgcolor="white" width="20rem" margin="0 auto">
+          <RadioGroup
+            defaultValue="cryptic"
+            sx={{
+              backgroundColor: "white",
+              margin: "0 auto",
+              borderRadius: "3px",
+            }}
+          >
+            <FormControlLabel
+              value="cryptic"
+              control={<Radio />}
+              label="Cryptic"
+              sx={{ paddingLeft: "1rem" }}
+            />
+            <FormControlLabel
+              value="human"
+              control={<Radio />}
+              label="Readable"
+              sx={{ paddingLeft: "1rem" }}
+            />
+          </RadioGroup>
+          <Button
+            sx={{
+              height: "2rem",
+              margin: "auto",
+              backgroundColor: "black",
+              color: "primary.main",
+              fontWeight: "heavy",
+              opacity: ".8",
+              ":hover": {
+                bgcolor: "primary.main",
+                color: "white",
+              },
+            }}
+          >
+            GENERATE
+          </Button>
+        </Grid>
       </Grid>
       <Footer backgroundColor="black"></Footer>
     </Grid>
